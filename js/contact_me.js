686847243be4f11c46218df213bb79e98b093f98
Metel Patel <metel.patel@bazaarvoice.com>
2022-08-08 12:23:03 -0500

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/metelpatel@gmail.com",
+                url: "",
                 method: "POST",
                 data: {
                     name: name,
